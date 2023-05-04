8af60df754d60363c58c66774fe73a15b8f3197e
Anuj-B10 <anbh2038@colorado.edu>
2023-05-04 12:44:46 -0600

    updated configs

--- cypress-integration-prod.config.ts
+++ cypress-integration-prod.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "q2cjw9",
   e2e: {
-    baseUrl: 'https://song-recommender-384417.uc.r.appspot.com',
     specPattern:'cypress/e2e/integration',
     setupNodeEvents(on, config) {
       // implement node event listeners here
