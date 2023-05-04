251a316f1686183cf519923284abea4626b5ac14
Anuj-B10 <anbh2038@colorado.edu>
2023-05-04 12:25:10 -0600

    update config files

--- cypress-acceptance-prod.config.ts
+++ cypress-acceptance-prod.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "q2cjw9",
   e2e: {
     baseUrl: 'https://song-recommender-384417.uc.r.appspot.com',
     specPattern:'cypress/e2e/acceptance',
