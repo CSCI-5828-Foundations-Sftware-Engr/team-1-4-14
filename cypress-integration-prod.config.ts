60975676df7cc05bbd36afb1fe69c71f6c0d2d24
Anuj-B10 <anbh2038@colorado.edu>
2023-05-04 13:39:53 -0600

    added baseurl

--- cypress-integration-prod.config.ts
+++ cypress-integration-prod.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "q2cjw9",
   e2e: {
+    baseUrl: 'http://localhost:4200',
     specPattern:'cypress/e2e/integration',
     setupNodeEvents(on, config) {
       // implement node event listeners here
