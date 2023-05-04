3ad7a24c145ec7cacea49852b0a0241f62988ef9
Sushravya Chandrakanth <SChandrakanth@cyberline.com>
2023-05-03 23:41:51 -0600

    added charts for data analysis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { MostPopularComponent } from './most-popular/most-popular.component';
 import { SongsGridComponent } from './songs-grid/songs-grid.component';
 import { SongInfoComponent } from './song-info/song-info.component';
+import { PopularSongsComponent } from './popular-songs/popular-songs.component';
 
 const routes: Routes = [
     { path: '', component: SongsGridComponent },
     { path: 'most-popular', component: MostPopularComponent },
+    { path: 'popular', component: PopularSongsComponent },
     { path: 'song/:id', component: SongInfoComponent }
 ];
 
